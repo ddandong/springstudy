94ce6a69961d746ca685c344c242e98de67c57ae
ddandong <demode@naver.com>
2016-01-15 02:19:39 +0900

    jpql2

--- hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot-jdbc/src/main/java/com/example/App.java
@@ -8,6 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -30,7 +33,16 @@ public class App implements CommandLineRunner
 		System.out.println(created + "is created!");
 	
 		//데이터 표시
-		customerRepository.findAllOrderByName().forEach(System.out::println);
+		//customerRepository.findAllOrderByName().forEach(System.out::println);
+		
+		//페이징 처리
+		Pageable pageable = new PageRequest(0,3);
+		Page<Customer> page = customerRepository.findAllOrderByName(pageable);
+		System.out.println("한 페이지당 데이터 수  = " + page.getSize());
+		System.out.println("현재 페이지  = " + page.getNumber());
+		System.out.println("전체 페이지 수  = " + page.getTotalPages());
+		System.out.println("전체 데이터 수  = " + page.getTotalElements());
+		page.getContent().forEach(System.out::println);
 	}
 	
     public static void main( String[] args )
--- hajiboot-jdbc/src/main/java/com/example/repository/CustomerRepository.java
+++ hajiboot-jdbc/src/main/java/com/example/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -79,5 +81,6 @@ public class CustomerRepository {
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 	@Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
-	List<Customer> findAllOrderByName();
+	//List<Customer> findAllOrderByName();
+	Page<Customer> findAllOrderByName(Pageable pageable);
 }
